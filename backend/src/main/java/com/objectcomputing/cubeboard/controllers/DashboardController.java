78b328c791f49e867684c54220e62bbe605682bb
Sergio del Amo <sergio.delamo@softamo.com>
2020-12-17 11:17:53 +0100

    add back SecurityRule import

--- backend/src/main/java/com/objectcomputing/cubeboard/controllers/DashboardController.java
+++ backend/src/main/java/com/objectcomputing/cubeboard/controllers/DashboardController.java
@@ -9,6 +9,7 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
